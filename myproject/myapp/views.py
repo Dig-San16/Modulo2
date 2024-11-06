18086142b39779e856f087eaf138df7bdf952930
Dig-San16 <digsantos.hoode@gmail.com>
2024-11-06 19:28:07 -0300

    Delete myproject directory

--- myproject/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here
-from django.http import HttpResponse
-from django.views import View
-
-class PostView(View):
-    def post(self, request, *args, **kwargs):
-        return HttpResponse("Hello, world!")
