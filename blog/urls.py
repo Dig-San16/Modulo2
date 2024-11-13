d0313daf948b7b3768baba5b1f8d8c83a5b4b12f
Dig-San16 <digsantos.hoode@gmail.com>
2024-11-13 12:41:02 -0300

    Delete blog directory

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import views
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail, name='post_detail'),
-]
-
